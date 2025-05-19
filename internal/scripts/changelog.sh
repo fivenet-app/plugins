c8769d703518d16ced9e4b106895ff6d1d8319d0
Alexander Trost <galexrt@googlemail.com>
2025-05-19 13:26:08 +0200

    ci: add release-it workflow and restructure push ci
    Signed-off-by: Alexander Trost <galexrt@googlemail.com>

---
+++ internal/scripts/changelog.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+NODE_VERSION=$(node -p -e "require('./package.json').version")
+
+if [ "$1" = "stdout" ]; then
+    pnpx git-cliff -o - --unreleased --tag "$NODE_VERSION"
+else
+    pnpx git-cliff -o './CHANGELOG.md' --tag "$NODE_VERSION"
+fi
